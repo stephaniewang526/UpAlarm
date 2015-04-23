2d03f9c6f867752d2389879e98756357792524a8
Bo Long <bl452@cornell.edu>
2015-04-23 06:17:52 -0400

    Basic Map functions added
    Map/heatmap is in Community
    Does not use LocationAPI yet, also need to see about overlaying rasters
    instead of markers

--- UpAlarm-Google play/app/src/main/java/com/stepahnieqianwang/upalarm/Community.java
+++ UpAlarm-Google play/app/src/main/java/com/stepahnieqianwang/upalarm/Community.java
@@ -5,15 +5,56 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import com.google.android.gms.location.LocationServices;
 
-public class Community extends ActionBarActivity {
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+import android.app.Activity;
+
+
+
+public class Community extends ActionBarActivity implements OnMapReadyCallback{
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
     }
 
+    @Override
+    public void onMapReady(GoogleMap map) {
+        float lat = 42.443f;
+        float lng = -76.5f;
+
+        LatLng ith = new LatLng(lat, lng);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(ith, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Ithaca")
+                .snippet("It's Gorges")
+                .position(ith));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
